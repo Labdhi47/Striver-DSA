71b6526ac9ce20f3bd04e4f65139d18997e8bed8
Labdhi Gathani <123727193+Labdhi47@users.noreply.github.com>
2023-06-27 11:51:05 +0530

    Create Pattern_2.py

---
+++ Build Up Logic Thinking/Pattern_2.py
@@ -0,0 +1,19 @@
+#   * 
+#   * * 
+#   * * * 
+#   * * * * 
+#   * * * * *
+
+class Solution:
+    def printTriangle(self, N):
+        for row in range(N):
+            for col in range(row+1):
+                print("*",end=" ")
+            print()    
+            
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTriangle(N)
