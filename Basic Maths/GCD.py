b39c10ead756ff2d6ea3f324bcd8d9669a113f52
Labdhi Gathani <123727193+Labdhi47@users.noreply.github.com>
2023-08-05 15:41:58 +0530

    Update GCD.py

--- Basic Maths/GCD.py
+++ Basic Maths/GCD.py
@@ -1,3 +1,4 @@
+#1
 num1=int(input())
 num2=int(input())
 gcd=1
@@ -5,3 +6,14 @@ for i in range(1,min(num1,mun2)):
     if num1%i==0 and num2%i==0:
         gcd=i
 print(gcd) 
+
+#2
+num1=int(input())
+num2=int(input())
+gcd=1
+i=1
+while(i<=num1 and i<=num2):
+    if num1%i==0 and num2%i==0:
+        gcd=i
+    i+=1    
+print(gcd)
