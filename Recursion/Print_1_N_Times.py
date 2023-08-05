14f5f3511cbe9c6bebe39a9f726118fb547ad58f
Labdhi Gathani <123727193+Labdhi47@users.noreply.github.com>
2023-08-05 17:22:43 +0530

    Update and rename Print_1_N_Times.py to Print_1_infinite_Times.py

--- Recursion/Print_1_N_Times.py
+++
@@ -1,5 +0,0 @@
-def print_n_times():
-    print("1")
-    print_n_times()
-    
-print_n_times()    
---
+++ Recursion/Print_1_infinite_Times.py
@@ -0,0 +1,5 @@
+def print_1_infinite_times():
+    print("1")
+    print_1_infinite_times()
+    
+print_1_infinite_times()    
